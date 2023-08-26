af6f50a4e333c42be1789d9013b361a0387c5c35
Hssain Aitkadir <hssain.aitkadir@gmail.com>
2023-08-26 11:33:37 +0100

    To Amine, hope this message finds you well. I want to inform you I'm officialy start working on the project. and this is my first commit on the project. The writer wishes you well
    Rida, I want to inform you too that I changed the client-side directory name to 'client' instead of 'my-app', Best regards

--- client/src/app/chatPage/page.tsx
+++ client/src/app/chatPage/page.tsx
@@ -1,25 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+// export const metadata: Metadata = {
+//   title: "Chat",
+// };
+
+"use client";
+
 import React from "react";
 import { Metadata } from "next";
 import { PageWrapper } from "../animationWrapper/pageWrapper";
 import ImgBackground from "../../../assets/icons/background.svg";
 import Image from "next/image";
+// import LeftBar from '@/components/chat/LeftBar';
+import LeftBar from "@/components/chat/leftBar";
 
-export const metadata: Metadata = {
-  title: "Chat",
-};
 
 export default function ChatPage() {
   return (
     <PageWrapper>
-      <div className="absolute inset-0 flex justify-center items-center h-screen w-screen">
+      {/* <div className="absolute inset-0 flex justify-center items-center h-screen w-screen">
         <div className="relative w-full h-full">
           <Image
             src={ImgBackground}
             alt="Background"
             className="object-cover w-full h-full"
           />
+          <LeftBar />
+
         </div>
-      </div>
+      </div> */}
+      <LeftBar />
     </PageWrapper>
   );
 }
