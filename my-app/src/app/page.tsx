2a57c2a5389b313116405435f001865846745178
The-Intruder <aminnaimi@hotmail.fr>
2023-08-28 21:52:00 +0100

    feat: :art: Still working on the Navbar, plus added the bg-image globally

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Metadata } from "next";
 import { PageWrapper } from "./animationWrapper/pageWrapper";
-import ImgBackground from "../../assets/icons/background.svg";
-import Image from "next/image";
-import WithSubnavigation from "../components/Navbar/Navbar"
  
 // TODO The background image shouldn't reload each time we change the route
 
@@ -15,12 +12,11 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <PageWrapper>
-      {/* <div className="absolute inset-0 flex justify-center items-center h-screen w-screen">
+      <div className="absolute inset-0 flex justify-center items-center h-screen w-screen">
         <div className="relative w-full h-full">
 
         </div>
-      </div> */}
+      </div>
     </PageWrapper>
   );
 }
-
--- my-app/src/components/Navbar/Navbar.tsx
+++ my-app/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { Box, Flex, Button, ButtonGroup, Spacer, Center, Text } from "@chakra-ui/react";
+import { Box, Flex, Button, ButtonGroup, Spacer, Center, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import Logo from "../../../assets/icons/Logo.svg";
 import WavesDivider from "../../../assets/icons/wavesOpacity.svg";
 import { motion } from 'framer-motion';
 import Image from 'next/image'
 import Link from "next/link";
 
-const WaveHeaderDivider: React.FC = () => {
-  return (
-    <Image src={WavesDivider} alt="wavesDivider" />
-  )
-}
 
 const MyButton: React.FC<{
   color: string,
@@ -22,23 +17,25 @@ const MyButton: React.FC<{
   return (
     <motion.div>
       <Button
-      className={"mx-2"}
+        className="mx-2 border-slate-50 border font-semibold"
         size={"md"}
         as={"a"}
         w={"6rem"}
         fontSize={"2xl"}
-        fontWeight={600}
-        href={href}
-        color={color}
-        bg={bg}
         boxShadow={"4px 4px 0px 0px var(--text-primary)"}
-        borderWidth={1}
-        borderColor={"var(--text-primary)"}
-        _hover={{}}
+
+        _hover={{
+          transform: 'translate(-2px, -2px)',
+          boxShadow: "6px 6px 0px 0px var(--text-primary)",
+        }}
         _active={{
           transform: 'translate(4px, 4px)',
           boxShadow: "0px 0px 0px 0px var(--text-primary)",
         }}
+
+        href={href}
+        color={color}
+        bg={bg}
       >
         {children}
       </Button>
@@ -49,62 +46,53 @@ const MyButton: React.FC<{
 const NavbarAuthButtons: React.FC = () => {
   return (
     <ButtonGroup className={'my-5 mx-10'} gap='2'>
-      <MyButton
-        color="white"
-        bg="var(--background-primary)"
-        href="#"
-      >
-        Log In
-      </MyButton>
-      <MyButton
-        color="var(--background-primary)"
-        bg="white"
-        href="#"
-      >
-        Sign Up
-      </MyButton>
+      {NAVBAR_BUTTONS.map((item, index) => {
+        return (
+          <MyButton
+            key={item.text + '-' + index}
+            color={item.color}
+            bg={item.bg}
+            href={item.href}
+          >
+            {item.text}
+          </MyButton>
+        )
+      })}
     </ButtonGroup>
   );
 }
 
-
 const NavbarLinks: React.FC = () => {
   return (
-    <Flex color={'white'}>
-      <Center w='90px'>
-        <Link href={"#"}>
-          <Text fontSize={"2xl"} fontWeight={500}>
-            Home
-          </Text>
-        </Link>
-      </Center>
-      <Center w='90px'>
-        <Link href={"#"}>
-          <Text fontSize={"2xl"} fontWeight={500}>
-            Game
-          </Text>
-        </Link>
-      </Center>
-      <Center w='90px'>
-        <Link href={"#"}>
-        <Text fontSize={"2xl"} fontWeight={500}>
-            Chat
-          </Text>
-        </Link>
-      </Center>
-    </Flex>
+    <motion.div>
+      <Flex color={'white'}>
+        <Wrap>
+          {NAVBAR_ITEMS.map((item, index) => {
+            return (
+              <WrapItem>
+                <Center w='90px'>
+                  <Link href={item.href} key={item.text + '-' + index}>
+                    <Text className="text-xl font-semibold">
+                      {item.text}
+                    </Text>
+                  </Link>
+                </Center>
+              </WrapItem>
+            )
+          })}
+        </Wrap>
+      </Flex>
+    </motion.div>
   )
 }
 
-
-
 const Navbar: React.FC = () => {
   return (
-      <Box>
+    <Box>
       <Flex minWidth='max-content' alignItems='center' gap='2' bg={"var(--background-primary)"}>
         <Box p='2'>
           <Link href={'#'}>
-            <Image className={'my-5 mx-10'} src={Logo} alt={"Logo"} width={150} height={150}/>
+            <Image className={'my-5 mx-10'} src={Logo} alt={"Logo"} width={150} height={150} />
           </Link>
         </Box>
         <NavbarLinks />
@@ -113,9 +101,47 @@ const Navbar: React.FC = () => {
           <NavbarAuthButtons />
         </Box>
       </Flex>
-      <Image src={WavesDivider} alt="wavesDivider" className={"w-screen h-12"}/>
+      <Image src={WavesDivider} alt="wavesDivider" className={"w-screen h-12"} />
     </Box>
   )
 }
- 
+
+let NAVBAR_BUTTONS: Array<{
+  text: string,
+  href: string,
+  color: string,
+  bg: string
+}> = [
+    {
+      text: "Log In",
+      href: "#",
+      color: "white",
+      bg: "var(--background-primary)"
+    },
+    {
+      text: "Sign Up",
+      href: "#",
+      color: "var(--background-primary)",
+      bg: "white",
+    },
+  ];
+
+let NAVBAR_ITEMS: Array<{
+  text: string,
+  href: string
+}> = [
+    {
+      text: "Home",
+      href: "#"
+    },
+    {
+      text: "Game",
+      href: "#"
+    },
+    {
+      text: "Chat",
+      href: "#"
+    }
+  ];
+
 export default Navbar;
