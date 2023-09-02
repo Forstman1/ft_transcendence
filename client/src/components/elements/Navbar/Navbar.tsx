a3e33bdb7f858bc97c77b187fe98eb76df20bd3f
The-Intruder <aminnaimi@hotmail.fr>
2023-09-02 19:23:25 +0100

    fix: :art: Improved the responsiveness on the navbar

--- client/src/components/elements/Navbar/Navbar.tsx
+++ client/src/components/elements/Navbar/Navbar.tsx
@@ -11,6 +11,10 @@ import { motion } from 'framer-motion';
 import Image from 'next/image'
 import Link from "next/link";
 import React from 'react';
+import { usePathname } from 'next/navigation';
+
+
+const GameRouter = ["/gamePage/gameFriendPage", "/gamePage/gameBotPage"]
 
 
 let NAVBAR_BUTTONS: Array<{
@@ -39,19 +43,15 @@ let NAVBAR_ITEMS: Array<{
 }> = [
     {
       text: "Home",
-      href: "#0"
+      href: "/"
     },
     {
       text: "Game",
-      href: "#0"
+      href: "/gamePage"
     },
     {
       text: "Chat",
-      href: "#0"
-    },
-    {
-      text: "About Us",
-      href: "#0"
+      href: "/chatPage"
     }
   ];
 
@@ -64,15 +64,8 @@ const MyButton: React.FC<{
   return (
     <motion.div>
       <Button as={"a"} href={href} size='sm'
-        className={`
-          ${backgroundColor}
-          ${color}
-          border-neutral-50 
-          text-xl rounded 
-          border 
-          border-current 
-          font-semibold
-        `}
+        className={`${backgroundColor} ${color} border-neutral-50 text-xl 
+          rounded border border-current font-semibold`}
         boxShadow={"0.2rem 0.2rem 0rem 0rem rgb(150,150,150)"}
         _hover={{}}
         _active={{
@@ -88,6 +81,7 @@ const MyButton: React.FC<{
 
 const NavbarAuthButtons: React.FC = () => {
   const breakpoint = useBreakpointValue({ base: "base", md: "md", lg: "lg" });
+
   return (
     <ButtonGroup className="flex flex-row justify-center space-x-5">
       {NAVBAR_BUTTONS.map((item: {
@@ -96,7 +90,6 @@ const NavbarAuthButtons: React.FC = () => {
         backgroundColor: string,
         href: string
       }, index: number) => {
-
         return (
           breakpoint === "base" && index === 1 ? null : <MyButton
             key={index}
@@ -113,20 +106,31 @@ const NavbarAuthButtons: React.FC = () => {
 }
 
 const NavbarLinks: React.FC = () => {
+  let path = usePathname();
+
+  // if (GameRouter.includes(path)) {
+  //   path = "/gamePage";
+  // }
   return (
     <motion.div>
-      <Flex color={'white'} className="w-full h-full items-center justify-start space-x-10">
+      <Flex color={'white'} className={`grid-cols-${NAVBAR_ITEMS.length} w-full h-full items-center justify-start space-x-10`}>
         {NAVBAR_ITEMS.map((item: {
           text: string,
           href: string
         }, index: number) => {
           return (
-            <Box key={index}>
+            <Box key={index} className='col-span-1'>
               <Center>
-                <Link href={item.href}>
+                <Link href={item.href} className="w-auto">
                   <Text className="text-xl font-semibold">
                     {item.text}
                   </Text>
+                  {path === item.href ? (
+                    <motion.span
+                      layoutId="underline"
+                      className="absolute w-5 h-1 bg-white rounded-full"
+                    />
+                  ) : null}
                 </Link>
               </Center>
             </Box>
@@ -151,23 +155,23 @@ const MenuLinks: React.FC = () => {
           boxShadow: "0rem 0rem 0rem 0rem rgb(20,20,20)",
         }}
       />
-      <MenuList className="bg-neutral-950 text-neutral-50 border-neutral-950 
+      <MenuList className="bg-neutral-900 text-neutral-50 border-neutral-950 
           text-xl rounded border border-current font-semibold">
-        <MenuItem >
-          Home
-        </MenuItem>
-        <hr className="border-neutral-600" />
-        <MenuItem >
-          Game
-        </MenuItem>
-        <hr className="border-neutral-600" />
-        <MenuItem >
-          Chat
-        </MenuItem>
-        <hr className="border-neutral-600" />
-        <MenuItem >
-          About Us
-        </MenuItem>
+        {NAVBAR_ITEMS.map((item: {
+          text: string,
+          href: string
+        }, index: number) => {
+          return (
+            <Box key={index}>
+              <MenuItem className="bg-neutral-900 text-neutral-50" as={"a"} href={item.href}>
+                <Text className="text-xl font-semibold">
+                  {item.text}
+                </Text>
+              </MenuItem>
+              {index != NAVBAR_ITEMS.length - 1 ? <hr className="w-full bg-neutral-50"></hr> : null}
+            </Box>
+          )
+        })}
       </MenuList>
     </Menu>
   )
@@ -205,23 +209,24 @@ const Navbar: React.FC = () => {
   }
   else {
     return (
-      <Box className="w-full h-20 bg-neutral-950 p-0 m-0">
-        <Flex className="w-full h-full items-center justify-between">
-          <Box className="w-1/4 h-auto">
-            <Center>
-              <Link href="/">
-                <Image src={Logo} alt="Logo" width={150} height={150} />
-              </Link>
-            </Center>
+      <Box className="w-full h-24 bg-neutral-950">
+        <Flex className="w-full h-full items-center grid-cols-4">
+          <Box className="col-span-1 ml-20 p-auto w-auto h-auto ">
+            <Link href="/">
+              <Image src={Logo} alt="Logo" width={150} height={150} />
+            </Link>
           </Box>
-          <Box className="w-2/4 h-auto">
+          <Box className="col-span-1 ml-20 w-auto h-auto">
             <NavbarLinks />
           </Box>
-          <Box className="w-1/4 h-auto">
-              <NavbarAuthButtons />
+          <Box className="col-span-1 m-auto w-auto h-auto">
+
+          </Box>
+          <Box className="col-span-1 mr-20 w-auto h-auto">
+            <NavbarAuthButtons />
           </Box>
         </Flex>
-        <Image src={WavesDivider} alt="WavesDivider" className="w-full h-5" />
+        <Image src={WavesDivider} alt="WavesDivider" className="w-full h-8" />
       </Box>
     )
   }
