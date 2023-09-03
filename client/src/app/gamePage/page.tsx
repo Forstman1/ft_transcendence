e78d57f47c909ab859a648f7256682f54a318d7e
Rida El fagrouch <rel-fagr@e3r3p1.1337.ma>
2023-09-03 13:43:49 +0100

    fix design issus in game page

--- client/src/app/gamePage/page.tsx
+++ client/src/app/gamePage/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { PageWrapper } from "../animationWrapper/pageWrapper";
-import { Button, Text, useDisclosure } from "@chakra-ui/react";
+import { Button, Text, useDisclosure, useBreakpointValue } from "@chakra-ui/react";
 import Image from "next/image";
 import Gamepad from "../../../assets/icons/gamepad.svg";
 import Robot from "../../../assets/icons/robot.svg";
@@ -15,6 +15,7 @@ import { motion } from "framer-motion";
 export default function GamePage() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [gameType, setGameType] = React.useState("");
+  const breakpoint = useBreakpointValue({ base: "base", md: "md", lg: "lg" });
 
   const handleBotClick = () => {
     setGameType("bot");
@@ -28,66 +29,54 @@ export default function GamePage() {
 
   return (
     <PageWrapper>
-      <div className="inset-0 flex justify-center items-center w-screen h-screen">
-        <div className="flex flex-row justify-center items-center top-0 left-0 w-full h-full">
-          <div className=" absolute justify-center items-center ">
-            <div className="flex flex-row justify-center items-center mx-auto">
-              <div className="flex flex-col justify-center  P-20 space-y-6">
-                <Text className=" flex text-emerald-300 font-bold text-2xl">
-                  EXPLORE THE GAME
-                </Text>
-                <Text className=" flex text-black font-bold text-6xl">
-                  It&apos;s time to enjoy the game
-                </Text>
-                <div className="flex flex-col justify-center items-center  space-y-6  mx-auto w-[400px] h-[300px] p-10">
-                  <Button
-                    colorScheme="teal"
-                    variant="outline"
-                    bg="white"
-                    size="lg"
-                    leftIcon={
-                      <Image
-                        src={Gamepad}
-                        alt="Gamepad"
-                        width={25}
-                        height={25}
-                      />
-                    }
-                    onClick={handleFriendClick}
-                  >
-                    Play with a friend
-                  </Button>
-                  <Button
-                    colorScheme="teal"
-                    variant="outline"
-                    bg="white"
-                    size="lg"
-                    leftIcon={
-                      <Image src={Robot} alt="Robot" width={25} height={25} />
-                    }
-                    onClick={handleBotClick}
-                  >
-                    Play with a robot
-                  </Button>
-                </div>
-              </div>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 360, scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 260,
-                  damping: 20,
-                }}
-              >
-                <Lottie
-                  animationData={animationData}
-                  className="w-full h-[900px] border-2 border-white rounded-[100%] shadow-xl min-w-[500px]"
-                />
-              </motion.div>
-            </div>
+      <div className="flex flex-row justify-center items-center mx-[10%] mt-[100px]">
+        <div className={`${breakpoint === "base" ? "absolute" : "flex"} flex-col justify-center  P-20 space-y-6 z-10`}>
+          <Text className=" flex text-emerald-300 font-bold text-2xl">
+            EXPLORE THE GAME
+          </Text>
+          <Text className=" flex text-black font-bold text-6xl">
+            It&apos;s time to enjoy the game
+          </Text>
+          <div className={`flex flex-col justify-center items-center  space-y-6  mx-auto w-[400px] h-[300px] p-10`}>
+            <Button
+              colorScheme="teal"
+              variant="outline"
+              bg="white"
+              size="lg"
+              leftIcon={
+                <Image src={Gamepad} alt="Gamepad" width={25} height={25} />
+              }
+              onClick={handleFriendClick}
+            >
+              Play with a friend
+            </Button>
+            <Button
+              colorScheme="teal"
+              variant="outline"
+              size="lg"
+              leftIcon={
+                <Image src={Robot} alt="Robot" width={25} height={25} />
+              }
+              onClick={handleBotClick}
+            >
+              Play with a robot
+            </Button>
           </div>
         </div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 360, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+        >
+          <Lottie
+            animationData={animationData}
+            className={`w-full h-[900px] border-2 border-white rounded-[100%] shadow-xl min-w-[370px] ${breakpoint === "base" ? "opacity-20" : ""}`} 
+          />
+        </motion.div>
       </div>
       <GameModesModal
         isOpen={isOpen}
