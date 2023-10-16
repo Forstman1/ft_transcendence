7aa4a0bfe8232dff2c9e8c8fb1f1bf28ea0d52a7
ridaelfagrouch <elfagrouchrida21@gmail.com>
2023-10-16 20:56:25 +0100

    dockerize the project

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as dotenv from 'dotenv';
+
+// dotenv.config({
+//   path: '../../.env',
+// });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
-
-  // setInterval(() => {
-  //   gameService.updateBallPosition();
-  // }, 1000 / 60);
 }
 bootstrap();
-
