f00a1d2bf9bdb1c5ec4ddb96d59bd689cc43ed64
The-Intruder <aminnaimi@hotmail.fr>
2023-11-27 19:53:30 +0100

    fix: :bug: fixed the RestrictedRoute bug

--- client/src/components/RestrictedRoute.tsx
+++ client/src/components/RestrictedRoute.tsx
@@ -1,18 +1,37 @@
 import { redirect } from 'next/navigation';
-import { useSelector } from 'react-redux';
-import { UserState } from '@/redux/slices/authUser/authUserSlice';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { fetchUserProfile } from '@/utils/functions/auth/fetchingUserData';
+import { Spinner } from '@chakra-ui/react'
+
 
 interface RestrictedRouteProps {
   children: React.ReactNode;
 }
 
 export default function RestrictedRoute({ children }: RestrictedRouteProps) {
-  const isAuthorized = useSelector((state: { authUser: UserState }) => state.authUser.isAuthenticated);
-
-  if (!isAuthorized) {
+  const { isLoading, isError, isSuccess } = useQuery({
+    queryKey: ['userProfile'],
+    queryFn: fetchUserProfile,
+    retry: false
+  });
+  if (isLoading) {
+    return (
+      <div
+        className="flex w-full min-h-fit
+        px-10 xl:px-30 2xl:px-40 3xl:px-50 4xl:px-60 5xl:px-70 6xl:px-80
+        pt-[6rem] md:pt-[10rem] xl:pt-[16rem] pb-[4rem] md:pb-[10rem]
+        justify-evenly justify-items-center
+        content-center items-center"
+      >
+        <Spinner size='xl' />
+      </div>
+    )
+  }
+  else if (isError) {
     redirect('/?unauthorized=true');
   }
-  else
+  else if (isSuccess) {
     return <>{children}</>
+  }
 }
